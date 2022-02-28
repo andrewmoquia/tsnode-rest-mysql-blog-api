57c6d6a1ae3c362d098b80d7d3ce6ad442b3a758
andrewmoquia <andrewmoquia09@gmail.com>
2022-03-01 01:26:52 +0800

    Updated post router.

--- src/api/post/post.router.ts
+++ src/api/post/post.router.ts
@@ -1,12 +1,26 @@
 import { PostActions } from './post.actions';
 import { Router } from 'express';
+import { PostValidator } from './post.validator';
 
 class PostRouter {
    private router = Router();
    private actions = new PostActions();
+   private validate = new PostValidator();
 
-   public createRouter() {
-      this.router.post('/create/post', this.actions.createPost);
+   createRouter() {
+      this.router.post(
+         '/create/post',
+         this.validate.reqBody(this.validate.PostSchema),
+         this.actions.createPost
+      );
+
+      this.router.get('/post/:id', this.actions.showPost);
+
+      this.router.get('/posts', this.actions.getAllPost);
+
+      this.router.put('/update/post/:id', this.actions.updatePost);
+
+      this.router.delete('/delete/post/:id', this.actions.deletePost);
 
       return this.router;
    }
